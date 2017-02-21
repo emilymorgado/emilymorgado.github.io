db0217b5c5b2aa5a1232c6a4552184364dfe1968
Emily Morgado <emilyannemorgado@gmail.com>
2017-02-20 19:23:13 -0800

    troubleshooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 function testButton() {
     // console.log("you clicked the button!");
+    console.log("new message");
     Front.openUrl('https://google.com');
+    console.log("window should have opened");
 }
